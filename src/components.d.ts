887cd1eb1937f39b09cad997f818ee70cd0ef450
Pineapple <1244476905@qq.com>
2022-02-03 23:28:41 +0800

    feat: 多选表单初始化

--- src/components.d.ts
+++ src/components.d.ts
@@ -5,15 +5,25 @@
 declare module 'vue' {
 	export interface GlobalComponents {
 		AAvatar: typeof import('ant-design-vue/es')['Avatar'];
+		AButton: typeof import('ant-design-vue/es')['Button'];
 		ACard: typeof import('ant-design-vue/es')['Card'];
+		ACheckbox: typeof import('ant-design-vue/es')['Checkbox'];
+		ACheckboxGroup: typeof import('ant-design-vue/es')['CheckboxGroup'];
 		ACol: typeof import('ant-design-vue/es')['Col'];
+		ADatePicker: typeof import('ant-design-vue/es')['DatePicker'];
+		AInput: typeof import('ant-design-vue/es')['Input'];
 		ALayout: typeof import('ant-design-vue/es')['Layout'];
 		ALayoutContent: typeof import('ant-design-vue/es')['LayoutContent'];
 		ALayoutFooter: typeof import('ant-design-vue/es')['LayoutFooter'];
 		ALayoutHeader: typeof import('ant-design-vue/es')['LayoutHeader'];
 		AMenu: typeof import('ant-design-vue/es')['Menu'];
 		AMenuItem: typeof import('ant-design-vue/es')['MenuItem'];
+		ARadio: typeof import('ant-design-vue/es')['Radio'];
+		ARadioGroup: typeof import('ant-design-vue/es')['RadioGroup'];
 		ARow: typeof import('ant-design-vue/es')['Row'];
+		ASelect: typeof import('ant-design-vue/es')['Select'];
+		ASelectOption: typeof import('ant-design-vue/es')['SelectOption'];
+		ASwitch: typeof import('ant-design-vue/es')['Switch'];
 		ATable: typeof import('ant-design-vue/es')['Table'];
 		CodeEditor: typeof import('./components/CodeEditor.vue')['default'];
 		HelloWorld: typeof import('./components/HelloWorld.vue')['default'];
--- src/pages/publish/components/MultipleForm.vue
+++ src/pages/publish/components/MultipleForm.vue
@@ -1,11 +1,62 @@
 <template>
-	<div>{{ data }}</div>
+	<a-form-model :model="form" :label-col="labelCol" :wrapper-col="wrapperCol">
+		<a-form-model-item label="Activity name">
+			<a-input v-model="form.name" />
+		</a-form-model-item>
+		<a-form-model-item label="Activity zone">
+			<a-select v-model="form.region" placeholder="please select your zone">
+				<a-select-option value="shanghai"> Zone one </a-select-option>
+				<a-select-option value="beijing"> Zone two </a-select-option>
+			</a-select>
+		</a-form-model-item>
+		<a-form-model-item label="Activity time">
+			<a-date-picker v-model="form.date1" show-time type="date" placeholder="Pick a date" style="width: 100%" />
+		</a-form-model-item>
+		<a-form-model-item label="Instant delivery">
+			<a-switch v-model="form.delivery" />
+		</a-form-model-item>
+		<a-form-model-item label="Activity type">
+			<a-checkbox-group v-model="form.type">
+				<a-checkbox value="1" name="type"> Online </a-checkbox>
+				<a-checkbox value="2" name="type"> Promotion </a-checkbox>
+				<a-checkbox value="3" name="type"> Offline </a-checkbox>
+			</a-checkbox-group>
+		</a-form-model-item>
+		<a-form-model-item label="Resources">
+			<a-radio-group v-model="form.resource">
+				<a-radio value="1"> Sponsor </a-radio>
+				<a-radio value="2"> Venue </a-radio>
+			</a-radio-group>
+		</a-form-model-item>
+		<a-form-model-item label="Activity form">
+			<a-input v-model="form.desc" type="textarea" />
+		</a-form-model-item>
+		<a-form-model-item :wrapper-col="{ span: 14, offset: 4 }">
+			<a-button type="primary" @click="onSubmit"> Create </a-button>
+			<a-button style="margin-left: 10px"> Cancel </a-button>
+		</a-form-model-item>
+	</a-form-model>
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue';
 
-const data = ref(0);
+const labelCol = ref({ span: 4 });
+const wrapperCol = ref({ span: 14 });
+
+const form = ref({
+	name: '',
+	region: undefined,
+	date1: undefined,
+	delivery: false,
+	type: [],
+	resource: '',
+	desc: '',
+});
+
+const onSubmit = () => {
+	console.log(form.value);
+};
 </script>
 
 <style lang="less" scoped></style>
