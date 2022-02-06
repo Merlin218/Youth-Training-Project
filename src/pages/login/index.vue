62abb42c3ebc890ff929b14947a05abf59048ebf
xuyuelin88117 <xuyuelin88117@gmail.com>
2022-02-06 22:46:20 +0800

    update: login

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,183 +1,114 @@
 <template>
-	<div class="login-vue" :style="bg">
-		<div class="container">
-			<p class="title">登录系统</p>
-			<div class="input-c">
-				<Input v-model="username" prefix="ios-contact" placeholder="用户名" clearable @on-blur="verifyUsername" />
-				<p class="error">{{ usernameError }}</p>
-			</div>
-			<div class="input-c">
-				<Input v-model="password" type="password" prefix="md-lock" placeholder="密码" clearable @on-blur="verifyPassword" />
-				<p class="error">{{ passwordError }}</p>
-			</div>
-			<Button :loading="isShowLoading" class="submit" type="primary" @click="loginByUserNameAndPassWord">登录</Button>
-			<!--<p class="account"><span @click="register">注册账号</span> | <span @click="forgetPwd">忘记密码</span></p>-->
+	<div class="login">
+		<div class="login-form">
+			<h1 style="color:#333;margin-bottom:40px;">Login</h1>
+			<!-- <Form :label-width="80" @keyup.native.enter="handleSubmit"> -->
+				<FormItem label="账号：">
+					<Input type="text" placeholder="Username" v-model="submit.username" />
+				</FormItem>
+				<FormItem label="密码：">
+					<Input type="password" v-model="submit.password" />
+				</FormItem>
+				<FormItem label="验证码：">
+					<div class="flex">
+						<Input type="text" style="width:40%;margin-right:30px" v-model="code" />
+						<canvas id="myCanvas"></canvas>
+						<Button type="text" style="margin-left:10px" @click="createCode">换一个</Button>
+					</div>
+				</FormItem>
+				<FormItem>
+					<Button type="primary" @click="handleSubmit">登陆</Button>
+					<Button @click="handleReset" style="margin-left: 8px">重置</Button>
+				</FormItem>
+			<!-- </Form> -->
 		</div>
 	</div>
 </template>
 
 <script>
-// import { LoginByUserNameAndPassWord } from "../../../api/login";
-
+//import mixins from '../mixins/index'
 export default {
-	name: 'Login',
+	mixins: [mixins],
 	data() {
 		return {
-			username: '',
-			password: '',
-			usernameError: '',
-			passwordError: '',
-			isShowLoading: false,
-			bg: {
-				// backgroundImage: 'url(' + require('../../../assets/bg04.jpg'),
-				backgroundPosition: 'center center',
-				backgroundRepeat: 'no-repeat',
-				backgroundSize: 'cover',
+			submit: {
+				username: "",
+				password: ""
 			},
+			createC: '',
+			code: '',
 		};
 	},
 	mounted() {
-		document.onkeydown = e => {
-			if (e.keyCode === 13) {
-				this.loginByUserNameAndPassWord();
-			}
-		};
+		this.createCode();
 	},
 	methods: {
-		verifyUsername() {
-			if (this.username === '') {
-				this.usernameError = '用户名不能为空！';
-			} else {
-				this.usernameError = '';
+		async handleSubmit() {
+			if (this.code !== this.createC) {
+				this.$Message.error('验证码输入错误')
+				return
+			}
+			this.$Spin.show();
+			let res// = await this.$ajax.post("/login", this.submit);
+			this.$Spin.hide();
+			if (res.data.token) {
+				localStorage.setItem("token", res.data.token);
+				localStorage.setItem("home_id", res.data.home_id);
+				// this.$router.push({ path: "/admin/articles-list" });
+				this.$Message.success('登录成功,即将跳转到主页')
+				setTimeout(() => this.$router.push({ path: `/home/${res.data.home_id}` }), 3000)
 			}
 		},
-		verifyPassword() {
-			if (this.password === '') {
-				this.passwordError = '密码不能为空！';
-			} else {
-				this.passwordError = '';
+		createCode() {
+			var c = document.getElementById("myCanvas");
+			var ctx = c.getContext("2d");
+			c.width = "120";
+			c.height = "40";
+			ctx.fillStyle = "#6adbcf";
+			ctx.fillRect(0, 0, 120, 40);
+			ctx.shadowOffsetX = 2;
+			ctx.shadowOffsetY = 2;
+			ctx.shadowBlur = 2;
+			ctx.shadowColor = "#666666";
+			ctx.font = "30px sans-serif";
+			var codes = "qwertyuioplkjhgfdsazxcvbnm1234567890";
+			var bgColor = ["#f5f5f5", "#666", "#bcbcbc", "yellow"];
+			for (let i = 0; i < 4; i++) {
+				var r = Math.floor(Math.random() * codes.length);
+				var x = 10 + i * 20,
+					y = 20 + Math.random() * 10;
+				this.createC += codes[r];
+				var b = Math.floor(Math.random() * bgColor.length);
+				ctx.fillStyle = bgColor[b];
+				ctx.fillText(codes[r], x, y);
 			}
 		},
-		faceLogin() {
-			this.$router.replace({ name: 'FaceLogin' });
-		},
-		loginByUserNameAndPassWord() {
-			if (this.username !== '' && this.password !== '') {
-				this.isShowLoading = true;
-				// return new Promise(((resolve, reject) => {
-				// 	LoginByUserNameAndPassWord(this.username, this.password).then(response => {
-				// 		if (response.status === 201) {
-				// 			document.onkeydown = undefined;
-				// 			this.$Message.success("登录成功，即将跳转到主页!");
-				// 			// 登录成功，设置用户信息
-				// 			this.$store.commit('setUser', {
-				// 				userId: response.data.userId,
-				// 				userName: response.data.userName,
-				// 				userGender: response.data.userGender,
-				// 				userEmail: response.data.userEmail,
-				// 				userRoleName: response.data.userRoleName
-				// 			});
-				// 			this.isShowLoading = false;
-				// 			this.$router.replace('index')
-				// 		} else {
-				// 			this.$Message.error(response.data.error);
-				// 			this.isShowLoading = false;
-				// 		}
-				// 	}).catch(error => {
-				// 		this.$Message.error(error.response.data.error);
-				// 		this.isShowLoading = false;
-				// 		reject(error);
-				// 	})
-				// }));
-			} else {
-				this.$Message.warning('请输入用户名和密码');
-			}
-			return 0;
-		},
-	},
+		handleReset() {
+			this.submit = {}
+		}
+	}
 };
 </script>
 
-<style>
-.login-vue {
-	height: 100%;
+<style >
+.login {
+	/* background: url("../assets/login.jpg") center center; */
+	background: #f8f8f9;
+	width: 100vw;
+	height: 100vh;
+	background-size: cover;
 	display: flex;
 	justify-content: center;
-	align-items: center;
-	color: #fff;
 }
-
-.login-vue .container {
-	background: rgba(255, 255, 255, 0.5);
-	width: 360px;
+.login-form {
 	text-align: center;
+	padding: 20px;
 	border-radius: 10px;
-	padding: 50px;
+	color: #fff !important;
+	margin-top: 4rem;
 }
-
-.login-vue .ivu-input {
-	background-color: transparent;
-	color: #fff;
-	outline: #fff;
-	border-color: #fff;
-}
-
-.login-vue ::-webkit-input-placeholder {
-	/* WebKit, Blink, Edge */
-	color: rgba(255, 255, 255, 0.8);
-}
-
-.login-vue :-moz-placeholder {
-	/* Mozilla Firefox 4 to 18 */
-	color: rgba(255, 255, 255, 0.8);
-}
-
-.login-vue ::-moz-placeholder {
-	/* Mozilla Firefox 19+ */
-	color: rgba(255, 255, 255, 0.8);
-}
-
-.login-vue :-ms-input-placeholder {
-	/* Internet Explorer 10-11 */
-	color: rgba(255, 255, 255, 0.8);
-}
-
-.login-vue .title {
-	font-size: 16px;
-	margin-bottom: 20px;
-}
-
-.login-vue .input-c {
-	margin: auto;
-	width: 200px;
-}
-
-.login-vue .error {
-	color: red;
-	text-align: left;
-	margin: 5px auto;
-	font-size: 12px;
-	padding-left: 30px;
-	height: 20px;
-}
-
-.login-vue .submit {
-	width: 200px;
-}
-
-.login-vue .account {
-	margin-top: 30px;
-}
-
-.login-vue .account span {
-	cursor: pointer;
-}
-
-.login-vue .ivu-icon {
-	color: #eee;
-}
-
-.login-vue .ivu-icon-ios-close-circle {
-	color: #777;
+.flex {
+	display: flex;
+	align-items: center;
 }
 </style>
