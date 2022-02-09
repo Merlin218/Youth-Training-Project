1ab25a191bbc18f10e3624491703d825640fce42
LiuKairui <tclkr@live.com>
2022-02-09 17:48:34 +0800

    fix: 表格组件

---
+++ src/components/TableEdit.vue
@@ -0,0 +1,39 @@
+<template>
+	<div>
+		<vxe-table class="mytable-style" :edit-config="{ trigger: 'click', mode: 'cell' }" :data="tableData.data">
+			<vxe-column v-for="item in tableData.cols" :key="item.cid" :field="item.cKey" :title="item.cname" :edit-render="item.editable !== false ? { autofocus: '.vxe-input--inner' } : null">
+				<template v-if="item.editable !== false" #edit="{ row }">
+					<vxe-input v-model="row[item.cKey]" @blur="handleUpdate()"></vxe-input>
+				</template>
+			</vxe-column>
+		</vxe-table>
+	</div>
+</template>
+
+<script lang="ts" setup>
+import { reactive } from 'vue';
+import { ProTable } from '../pages/preproccess/preproccess/ProTable';
+
+const props = defineProps<{
+	table: ProTable;
+}>();
+const emit = defineEmits<{
+	(e: 'tableDataChange', tableData: ProTable): void;
+}>();
+
+const tableData = reactive(props.table);
+function handleUpdate() {
+	tableData.data.forEach(d => {
+		Object.keys(d).forEach(k => {
+			if (!Object.is(+d[k], NaN)) d[k] = +d[k];
+		});
+	});
+	emit('tableDataChange', tableData);
+}
+</script>
+
+<script lang="ts">
+export default {
+	name: 'TableEdit',
+};
+</script>
